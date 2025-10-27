44252589e59b3f27fc809511cba17009391ba01d
fron-tn <frontn365@gmail.com>
2025-10-27 12:44:33 +0200

    Add files via upload

---
+++ Chapter 2/2_name.py
@@ -0,0 +1,34 @@
+name = "Smo King Guap"
+print(name.title())
+
+print(name.upper())
+print(name.lower())
+
+
+first_name = "Smo"
+last_name = "King"
+full_name = f"Hello, {first_name} {last_name}. How are you finding Python so far?"
+print(full_name.title())
+
+print("\n")
+print("Python")
+print("\tPython")
+
+print("Languages:\nPython\nJavaScript\nHTMX")
+
+print("Languages: \n\tPython\n\tJavaScript\n\tHTMX")
+
+
+print("\n")
+fav_language = "       Python    "
+print(fav_language)
+print(fav_language.lstrip())
+
+print(fav_language.strip())
+
+
+
+nostarch_url = "https://nostarch.com"
+print(nostarch_url)
+print(nostarch_url.removeprefix("https://"))
+print(nostarch_url)
---
+++ Chapter 2/3_numbers.py
@@ -0,0 +1,40 @@
+print(f"2 + 3 = {2 + 3}")
+
+print(f"3 - 2 = {3 - 2}")
+
+print(f"3 / 2 = {3 / 2}")
+
+print(f"2 * 3 = {2 * 3}")
+
+print(f"3 ** 2 = {3 ** 2}")
+print(f"3 ** 3 = {3 ** 3}")
+
+print(f"2 + 3 * 4 = {2 + 3 * 4}")
+
+print(f"(2 + 3) * 4 = {(2 + 3) * 4}")
+
+print("\n")
+print(f"0.1 + 0.1 = {0.1 + 0.1}")
+print(f"0.2 + 0.2 = {0.2 + 0.2}")
+print(f"2 * 0.1 = {2 * 0.1}")
+print(f"2 * 0.2 = {2 * 0.2}")
+
+print("\n")
+print(f'1 + 2.0 = {1 + 2.0}')
+print(f'2 * 3.0 = {2 * 3.0}')
+print(f'3.0 ** 2 = {4 / 2}')
+
+print("\n")
+universe_age = 14_000_000_000
+print(universe_age)
+
+print("\n")
+x, y, z = 0, 0, 0
+print(f"x = {x}\ny = {y}\nz = {z}")
+
+print("\n")
+MAX_CONNECTIONS = 5_000
+print(MAX_CONNECTIONS)
+
+MAX_CONNECTIONS = 50_000
+print(MAX_CONNECTIONS)
